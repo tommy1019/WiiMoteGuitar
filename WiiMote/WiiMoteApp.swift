d662be3db3623c26228a2b6d01cd4ce34b9ef347
Tommy <tommy.stanley96@gmail.com>
2020-12-06 16:15:04 -0600

    Initial Commit

---
+++ WiiMote/WiiMoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiiMoteApp.swift
+//  WiiMote
+//
+//  Created by Tommy Stanley on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct WiiMoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
